f91e9916e3ec635f1ef90eef2f652278e546c872
isnieto <ismael.nieto@gmail.com>
2021-08-08 15:33:21 +0200

    Added DeleteCompany

--- src/app/controllers/companies.js
+++ src/app/controllers/companies.js
@@ -3,27 +3,50 @@ const prisma = require("../../../prisma/indexPrisma");
 const apiResponse = require("../services/utils.js");
 
 // Retrieve all companys from datadabe
-
 exports.findCompanies = async (req, res) => {
-  const allCompanies = await prisma.company.findMany();
-  if (allCompanies.length !== 0) {
-    res
-      .status(200)
-      .json(
-        apiResponse({ message: "Companies available", data: allCompanies })
-      );
-  } else if (allCompanies.length === 0) {
-    res.status(204).json(apiResponse({ message: "No data found found" }));
-  } else {
-    res.status(500).json(apiResponse({ message: "Server error" }));
+  try {
+    const allCompanies = await prisma.company.findMany();
+    if (allCompanies.length !== 0) {
+      res
+        .status(200)
+        .json(
+          apiResponse({ message: "Companies available", data: allCompanies })
+        );
+    } else {
+      res.status(204).json(apiResponse({ message: "No data found found" }));
+    } 
+  } catch (error) {
+    res.status(404).json(apiResponse({ errors: error }));
   }
 };
 
+// Create one company in table company
 exports.createCompany = async (req, res) => {
+  try {
+    let data = req.body;
+    await prisma.company.create({ data: { ...data } });
+    // Response 201: The request has succeeded and a new resource has been created as a result.
+    res.status(201).json(apiResponse({ message: "Company registered" }));
+  } catch (error) {
+    res.status(404).json(apiResponse({ errors: error }));
+  }
+};
 
-  let data = req.body;
-  let compData = await prisma.company.create({data: {...data}})
-  res.status(200).json(apiResponse({message: "Company registered"}))
-  //console.log("DATA", compData)
-
-}
+// Delete a company by Id
+exports.deleteCompany = async (req, res) => {
+  // Check that the request isn't empty
+  try {
+    let compId = parseInt(req.params.id);
+    let deletedCompany = await prisma.company.delete({
+      where: { id: compId },
+    });
+    res.status(200).json(
+      apiResponse({
+        message: "Company deleted succesfully",
+        data: deletedCompany,
+      })
+    );
+  } catch (error) {
+    res.status(404).json(apiResponse({ errors: error }));
+  }
+};
--- src/app/routes/companies.js
+++ src/app/routes/companies.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const companiesController = require("../controllers/companies");
 
+
+// Routes
 router.get('/', companiesController.findCompanies);
 router.post('/addone', companiesController.createCompany);
 //router.post('/add', jobsController.create);
-/* router.get('/:id', auditController.audit_get_byID);
-router.post('/', auditController.audit_create);
-router.delete('/:id', auditController.audit_delete); */
+router.delete('/:id', companiesController.deleteCompany);
+
+
+
 
 module.exports = router;
